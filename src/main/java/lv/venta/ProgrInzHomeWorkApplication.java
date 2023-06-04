e01c5058fc16b7cee4526535eab442fa11edfb29
Paula <paula.cielava1998@gmail.com>
2023-06-04 19:36:01 +0300

    New: Home Work project

---
+++ src/main/java/lv/venta/ProgrInzHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrInzHomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrInzHomeWorkApplication.class, args);
+	}
+
+}
