ec98dba893cb4791738c73ff4167fee66cfcf178
Paula <paula.cielava1998@gmail.com>
2023-06-05 13:37:37 +0300

    Add: CommandLineRunner and all reposotories

--- src/main/java/lv/venta/ProgrInzHomeWorkApplication.java
+++ src/main/java/lv/venta/ProgrInzHomeWorkApplication.java
@@ -1,7 +1,16 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.repos.ICashierRepo;
+import lv.venta.repos.ICityRepo;
+import lv.venta.repos.IDriverRepo;
+import lv.venta.repos.IPersonRepo;
+import lv.venta.repos.ITicketRepo;
+import lv.venta.repos.ITripRepo;
 
 @SpringBootApplication
 public class ProgrInzHomeWorkApplication {
@@ -9,5 +18,34 @@ public class ProgrInzHomeWorkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProgrInzHomeWorkApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner testModel (ICashierRepo cashierRepo, ICityRepo cityRepo, IDriverRepo driverRepo, IPersonRepo personRepo, ITicketRepo ticketRepo, ITripRepo tripRepo) {
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				
+				
+				
+				
+				
+				
+				
+				
+				
+			}
+		};
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
